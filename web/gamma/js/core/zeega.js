782335138b989effd39e0a1b39d6f7f9dbd2ad27
Joseph Bergen <jbergen@gmail.com>
2011-11-07 17:25:23 +0000

    added copy to next node button to layers

--- web/gamma/js/core/zeega.js
+++ web/gamma/js/core/zeega.js
@@ -405,6 +405,13 @@ var Zeega = {
 		
 	},
 	
+	copyLayerToNextNode : function(layer)
+	{
+		console.log('copy to next layer');
+		var nextNode = this.getRightNode();
+		if (nextNode) this.addLayerToNode(nextNode,layer);
+	},
+	
 	persistLayerOverNodes : function(layer)
 	{
 		console.log('peristing');
