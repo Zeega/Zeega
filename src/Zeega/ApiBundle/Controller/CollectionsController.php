9c995bbfd149b4cee96f58246abddd808da12029
Luís Brandão <luisbrandao@gmail.com>
2011-12-05 16:00:30 -0500

    Added the ability to add items to collections using the API

--- src/Zeega/ApiBundle/Controller/CollectionsController.php
+++ src/Zeega/ApiBundle/Controller/CollectionsController.php
@@ -1,30 +1,13 @@
 <?php
-//test
 namespace Zeega\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Zeega\IngestBundle\Entity\Item;
 
-//
-//use Doctrine\ORM\EntityRepository;
-//use Zeega\UserBundle\Entity\User;
-//use Zeega\EditorBundle\Entity\Playground;
-
 class CollectionsController extends Controller
 {
-    private function populateQueryGlobalParameters($request, $query)
-    {
-        $query["page"]  = $request->query->get('page');      //  string
-		$query["limit"] = $request->query->get('limit');     //  string
-		
-		//  set defaults for missing parameters  
-		if(!isset($query['page']))          $query['page'] = 0;
-		if(!isset($query['limit']))         $query['limit'] = 100;
-		if($query['limit'] > 100) 	        $query['limit'] = 100;
-    }
-    
-    //  "get_collections"    [GET] /collections
+    //  get_collections GET    /api/collections.{_format}
     public function getCollectionsAction()
     {
         $query = array();
@@ -57,111 +40,8 @@ class CollectionsController extends Controller
         // return the results
         return $response;
     }    
-
-    // "new_collections"    [POST] /api/collections
-    public function postCollectionsAction()
-    {
-        $user = $this->get('security.context')->getToken()->getUser();
-        if($user == "anon.")
-        {
-            $em = $this->getDoctrine()->getEntityManager();
-            $user = $em->getRepository('ZeegaUserBundle:User')->find(21);
-        }
-            
-        $item = new Item();
-        
-        $item->setTitle('My new collection');
-        $item->setType('Collection');
-        $item->setSource('Collection');
-        $item->setUser($user);
-        $item->setUri('collectionurl');
-        $item->setAttributionUri('zeega.org');
-        $item->setChildItemsCount(0);
-        $item->setMediaCreatorUsername($user->getUsername());
-        $item->setMediaCreatorRealname($user->getDisplayName());
-        
-        $em = $this->getDoctrine()->getEntityManager();
-        $em->persist($item);
-        $em->flush();
-
-        return new Response($item->getId());
-    }
     
-    public function postCollectionsItemsAction()
-    {
-        $user = $this->get('security.context')->getToken()->getUser();
-        if($user == "anon.")
-        {
-            $em = $this->getDoctrine()->getEntityManager();
-            $user = $em->getRepository('ZeegaUserBundle:User')->find(21);
-        }
-        
-        $em = $this->getDoctrine()->getEntityManager();
-        $request = $this->getRequest();        
-        $new_items = $request->request->get('new_items');
-        $new_items = explode(",", $new_items);
-        
-        $collection_item = new Item();
-        
-        foreach($new_items as $item)
-        {
-            $child_entity = $em->getRepository('ZeegaIngestBundle:Item')->find($item);
-
-            if (!$child_entity) 
-            {
-                throw $this->createNotFoundException('Unable to find Item entity.');
-            }    
-            
-            $collection_item->addItem($child_entity);
-        }
-        $em->persist($collection_item);
-        $em->flush();
-        return new Response(json_encode($collection_item));
-    }
-    
-    
-    // "vote_user_comment"    [PUT] /users/{slug}/comments/{id}/vote
-    public function putCollectionsItemsAction($project_id, $items_id)
-    {
-        $em = $this->getDoctrine()->getEntityManager();
-
-        $entity = $em->getRepository('ZeegaIngestBundle:Item')->find($project_id);
-        
-        if (!$entity) 
-        {
-            throw $this->createNotFoundException('Unable to find Collection entity.');
-        }
-
-        $items_list = explode(",", $items_id);
-        
-        // this is terrible...
-        foreach($items_list as $item)
-        {
-            $child_entity = $em->getRepository('ZeegaIngestBundle:Item')->find($item);
-
-            if (!$child_entity) 
-            {
-                throw $this->createNotFoundException('Unable to find Item entity.');
-            }    
-            
-            $entity->addItem($child_entity);
-        } 
-        
-        $items_list->setTitle('My new collection');
-        $items_list->setType('Collection');
-        $items_list->setSource('Collection');
-        $items_list->setUser($user);
-        $items_list->setUri('collectionurl');
-        $items_list->setAttributionUri('zeega.org');
-        $items_list->setChildItemsCount(0);
-        $items_list->setMediaCreatorUsername($user->getUsername());
-        $items_list->setMediaCreatorRealname($user->getDisplayName());
-
-        $em = $this->getDoctrine()->getEntityManager();
-        $em->persist($items_list);
-        $em->flush();
-    }
-    
+    // get_collection GET    /api/collections/{id}.{_format}
     public function getCollectionAction($id)
     {
         $em = $this->getDoctrine()->getEntityManager();
@@ -177,6 +57,7 @@ class CollectionsController extends Controller
         return $response;
     }
     
+    // get_collection_items     GET   /api/collections/{id}/items.{_format}
     public function getCollectionItemsAction($id)
     {
         $em = $this->getDoctrine()->getEntityManager();
@@ -206,4 +87,138 @@ class CollectionsController extends Controller
         // return the results
         return $response;
     }   
+        
+    // post_collections POST   /api/collections.{_format}
+    public function postCollectionsAction()
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+        if($user == "anon.")
+        {
+            $em = $this->getDoctrine()->getEntityManager();
+            $user = $em->getRepository('ZeegaUserBundle:User')->find(21);
+        }
+            
+        $item = new Item();
+        
+        $item->setTitle('My new collection');
+        $item->setType('Collection');
+        $item->setSource('Collection');
+        $item->setUser($user);
+        $item->setUri('collectionurl');
+        $item->setAttributionUri('zeega.org');
+        $item->setChildItemsCount(0);
+        $item->setMediaCreatorUsername($user->getUsername());
+        $item->setMediaCreatorRealname($user->getDisplayName());
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($item);
+        $em->flush();
+
+        return new Response($item->getId());
+    }
+    
+    // post_collections_items   POST   /api/collections/items.{_format}
+    public function postCollectionsItemsAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $request_data = $this->getRequest()->request;        
+        
+        $new_collection = $this->populateCollectionWithRequestData($request_data);
+        
+        if (!$new_collection) 
+        {
+            throw $this->createNotFoundException('Unable to create the collection.');
+        }
+        
+        $em->persist($new_collection);
+        $em->flush();
+        
+        return new Response(json_encode($new_collection));
+    }
+    
+    // put_collections_items   PUT    /api/collections/{project_id}/items.{_format}
+    public function putCollectionsItemsAction($project_id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('ZeegaIngestBundle:Item')->find($project_id);
+
+        if (!$entity) 
+        {
+            throw $this->createNotFoundException('Unable to find Collection entity.');
+        }
+        $items_list = $this->getRequest()->request->get('newItemIDS');
+
+        // this is terrible...
+        foreach($items_list as $item)
+        {
+            $child_entity = $em->getRepository('ZeegaIngestBundle:Item')->find($item);
+
+            if (!$child_entity) 
+            {
+                throw $this->createNotFoundException('Unable to find Item entity.');
+            }    
+            
+            $entity->addItem($child_entity);            
+        }
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($entity);
+        $em->flush();
+        
+        return new Response($entity->getId());
+    }
+   
+    // Private methods 
+    
+    private function populateCollectionWithRequestData($request_data)
+    {    
+        $user = $this->get('security.context')->getToken()->getUser();
+        if($user == "anon.")
+        {
+            $em = $this->getDoctrine()->getEntityManager();
+            $user = $em->getRepository('ZeegaUserBundle:User')->find(1);
+        }
+        
+        if (!$request_data) 
+            throw $this->createNotFoundException('Collection object is not defined.');
+        
+        $title = $request_data->get('title');
+        $new_items = $request_data->get('newItemIDS');
+        
+        $logger = $this->get('logger');
+        $logger->info("REQUEST DATA BELOW");
+        $logger->info(json_encode($this->getRequest()));
+            
+        if (!isset($title)) 
+            throw $this->createNotFoundException('Collection title is not defined.');
+
+        $collection = new Item();
+        $collection->setType('Collection');
+        $collection->setSource('Collection');
+        $collection->setUri('http://zeega.org');
+        $collection->setAttributionUri("http://zeega.org");
+        $collection->setUser($user);
+        $collection->setChildItemsCount(0);
+        $collection->setMediaCreatorUsername($user->getUsername());
+        $collection->setMediaCreatorRealname($user->getDisplayName());
+        $collection->setTitle($title);
+        
+        if (isset($new_items))
+        {
+            foreach($new_items as $item)
+            {
+                $child_entity = $em->getRepository('ZeegaIngestBundle:Item')->find($item);
+
+                if (!$child_entity) 
+                {
+                    throw $this->createNotFoundException('Unable to find Item entity.');
+                }    
+
+                $collection->addItem($child_entity);
+            }
+        }
+        
+        return $collection;
+    }
 }
