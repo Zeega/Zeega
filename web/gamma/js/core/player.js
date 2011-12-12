f5f8a7b06d15673198f2e5d78381951801e9478d
Joseph Bergen <jbergen@gmail.com>
2011-12-12 20:39:50 +0000

    added zeega icon to the player

--- web/gamma/js/core/player.js
+++ web/gamma/js/core/player.js
@@ -778,6 +778,7 @@ var Player = {
 	getTemplate : function()
 	{
 		html =	 	"<div id='preview-wrapper'><div id='zeega-player'>";
+		html +=			"<div id='preview-logo' class='player-overlay'><img src='../../../gamma/images/z-logo-128.png'height='60px'/></div>";
 		html += 		"<div id='preview-left' class='hidden preview-nav-arrow preview-nav'>";
 		html += 			"<div class='arrow-background'></div>";
 		html += 			"<img  height='75' width='35' onclick='Player.goLeft();return false'>";
