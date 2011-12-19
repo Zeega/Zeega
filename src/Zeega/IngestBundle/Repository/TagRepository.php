879e7e45ce8a4354259b92907ad9a531e09261bf
Luis Brandao <luisbrandao@gmail.com>
2011-12-19 12:54:03 -0500

    Limited the number of tags returned by the API

---
+++ src/Zeega/IngestBundle/Repository/TagRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+// src/Zeega/IngestBundle/Repository/TagRepository.php
+namespace Zeega\IngestBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class TagRepository extends EntityRepository
+{
+    public function findPaginated($limit,$offset)
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+
+        // search query
+        $qb->select('t')
+           ->from('ZeegaIngestBundle:Tag', 't')
+           ->orderBy('t.date_created','DESC')
+      	   ->setMaxResults($limit)
+      	   ->setFirstResult($offset);
+           
+        // execute the query
+        return $qb->getQuery()->getArrayResult();
+    }
+}
+
