3d899ba9909e31a66510f221f9f1b5018d992642
Luis Brandao <luisbrandao@gmail.com>
2012-01-08 17:48:24 +0000

    Removed tabs from Japanese items text

--- src/Zeega/ApiBundle/Helpers/ResponseHelper.php
+++ src/Zeega/ApiBundle/Helpers/ResponseHelper.php
@@ -14,6 +14,7 @@ class ResponseHelper
         //$renderedTwig = preg_replace('/\s+/','',$renderedTwig);
         $renderedTwig = preg_replace(array('/\s{2,}/','/\n/','/\p{Zl}/'), '',$renderedTwig);
         $renderedTwig = preg_replace(array('/\s:\s/'), ':',$renderedTwig);
+        $renderedTwig = preg_replace('/\s+/', '', $renderedTwig);
         
         //array("\r", "\r\n", "\n")
         $response = new Response($renderedTwig);
