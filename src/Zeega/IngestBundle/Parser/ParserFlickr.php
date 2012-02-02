fe5ae9cc007192a8ddb4c2fe23ffe0d737e05337
Luis Brandao <luisbrandao@gmail.com>
2012-02-02 00:26:19 -0500

    Widget refactoring: flickr is now working again after a bug fix

--- src/Zeega/IngestBundle/Parser/ParserFlickr.php
+++ src/Zeega/IngestBundle/Parser/ParserFlickr.php
@@ -42,11 +42,88 @@ class ParserFlickr extends ParserAbstract
      * @param String  $url  The url to be checked.
 	 * @return boolean|success
      */
-	public function parseSingleItem($url)
+	public function parseSingleItem($url,$itemId)
 	{
-		$id = $this->getItemId($url);
-		$item = $this->parseItem($id);
-		return parent::returnResponse($item, true);
+		$f = new \Phpflickr_Phpflickr('97ac5e379fbf4df38a357f9c0943e140');
+		$info = $f->photos_getInfo($itemId);
+		$size = $f->photos_getSizes($itemId);
+
+		if(is_array($info)&&is_array($size)) // why?
+		{
+			$item = new Item();
+			$metadata = new Metadata();
+			$media = new Media();
+			$tags = array();
+
+			$item->setAttributionUri($info['urls']['url'][0]['_content']);
+
+			if($info['tags'])
+			{
+				foreach($info['tags']['tag'] as $tag)
+				{
+					array_push($tags, ucwords(strtolower($tag['raw'])));
+				}
+				$attr['tags']=$tags;
+			}
+			else
+			{
+				$attr['tags']='';
+			}
+
+			foreach ($size as $s)
+			{
+				$sizes[$s['label']]=array('width'=>$s['width'],'height'=>$s['height'],'source'=>$s['source']);
+			}	
+			//return $sizes;
+			$item->setThumbnailUrl($sizes['Square']['source']);
+			$metadata->setThumbnailUrl($sizes['Small']['source']);
+
+			$attr = array('farm'=>$info['farm'],'server'=>$info['server'],'id'=>$info['id'],'secret'=>$info['secret']);
+			if(isset($sizes['Original'])) $attr['originalsecret']=$info['originalsecret'];
+
+			if(isset($sizes['Large']))$itemSize='Large';
+			elseif(isset($sizes['Original'])) $itemSize='Original';
+			elseif(isset($sizes['Medium'])) $itemSize='Medium';
+			else $itemSize='Small';
+
+			if($info['dates']['taken']) $item->setMediaDateCreated(new DateTime($info['dates']['taken']));
+
+			$item->setUri($sizes[$itemSize]['source']);
+			$item->setChildItemsCount(0);
+			$media->setWidth($sizes[$itemSize]['width']);
+			$media->setHeight($sizes[$itemSize]['height']);
+
+			$attr['sizes']=$sizes;
+			$item->setDescription($info['description']);
+
+			if($info['license'])$metadata->setLicense(self::$license[$info['license']]);
+			else $metadata->setLicense('All Rights Reserved');
+
+			if($info['owner']['username']) $item->setMediaCreatorUsername($info['owner']['username']);
+			else $item->setMediaCreatorUsername($info['owner']['realname']);
+			$item->setMediaCreatorRealname($info['owner']['realname']);
+			$attr['creator_nsid']=$info['owner']['nsid'];
+			$item->setTitle($info['title']);
+
+			if(array_key_exists ('location',$info)){
+				if($info['location']['latitude']){
+					$item->setMediaGeoLatitude($info['location']['latitude']);
+					$item->setMediaGeoLongitude($info['location']['longitude']);
+				}
+			}
+
+			$metadata->setArchive('Flickr'); 
+			$item->setType('Image');
+			$item->setSource('Image');
+			$metadata->setAttributes($attr);
+			$item->setMedia($media);
+			$item->setMetadata($metadata);
+
+			return parent::returnResponse($item, true);
+		}
+		else{
+			return parent::returnResponse($item, false);
+		}
 	}
 	
 	/**
@@ -191,110 +268,4 @@ class ParserFlickr extends ParserAbstract
 	}
 
 	/*  PRIVATE METHODS */
-
-	private function getItemId($url)
-	{
-		if(strstr($url,'flickr.com')&&strstr($url,'/photos/')&&!strstr($url,'/sizes/'))
-		{
-			if(strstr($url,'sets'))
-			{
-				$split = explode('/sets/',$url);
-				$split = explode('/',$split[1]);
-				$id = $split[0];
-			}
-			else 
-			{
-				$split = explode('/photos/',$url);
-				$split = explode('/',$split[1]);
-				$id = $split[1];
-			}
-
-			return $id;
-		}
-		return -1;
-	}
-	
-	private function parseItem($itemId)
-	{
-		$f = new \Phpflickr_Phpflickr('97ac5e379fbf4df38a357f9c0943e140');
-		$info = $f->photos_getInfo($itemId);
-		$size = $f->photos_getSizes($itemId);
-		
-		if(is_array($info)&&is_array($size)) // why?
-		{
-			$item = new Item();
-			$metadata = new Metadata();
-			$media = new Media();
-			$tags = array();
-			
-			$item->setAttributionUri($info['urls']['url'][0]['_content']);
-			
-			if($info['tags'])
-			{
-				foreach($info['tags']['tag'] as $tag)
-				{
-					array_push($tags, ucwords(strtolower($tag['raw'])));
-				}
-				$attr['tags']=$tags;
-			}
-			else
-			{
-				$attr['tags']='';
-			}
-			
-			foreach ($size as $s)
-			{
-				$sizes[$s['label']]=array('width'=>$s['width'],'height'=>$s['height'],'source'=>$s['source']);
-			}	
-			//return $sizes;
-			$item->setThumbnailUrl($sizes['Square']['source']);
-			$metadata->setThumbnailUrl($sizes['Small']['source']);
-			
-			$attr = array('farm'=>$info['farm'],'server'=>$info['server'],'id'=>$info['id'],'secret'=>$info['secret']);
-			if(isset($sizes['Original'])) $attr['originalsecret']=$info['originalsecret'];
-
-			if(isset($sizes['Large']))$itemSize='Large';
-			elseif(isset($sizes['Original'])) $itemSize='Original';
-			elseif(isset($sizes['Medium'])) $itemSize='Medium';
-			else $itemSize='Small';
-			
-			if($info['dates']['taken']) $item->setMediaDateCreated(new DateTime($info['dates']['taken']));
-			
-			$item->setUri($sizes[$itemSize]['source']);
-			$item->setChildItemsCount(0);
-			$media->setWidth($sizes[$itemSize]['width']);
-			$media->setHeight($sizes[$itemSize]['height']);
-			
-			$attr['sizes']=$sizes;
-			$item->setDescription($info['description']);
-			
-			if($info['license'])$metadata->setLicense(self::$license[$info['license']]);
-			else $metadata->setLicense('All Rights Reserved');
-		
-			if($info['owner']['username']) $item->setMediaCreatorUsername($info['owner']['username']);
-			else $item->setMediaCreatorUsername($info['owner']['realname']);
-			$item->setMediaCreatorRealname($info['owner']['realname']);
-			$attr['creator_nsid']=$info['owner']['nsid'];
-			$item->setTitle($info['title']);
-			
-			if(array_key_exists ('location',$info)){
-				if($info['location']['latitude']){
-					$item->setMediaGeoLatitude($info['location']['latitude']);
-					$item->setMediaGeoLongitude($info['location']['longitude']);
-				}
-			}
-		
-			$metadata->setArchive('Flickr'); 
-			$item->setType('Image');
-			$item->setSource('Image');
-			$metadata->setAttributes($attr);
-			$item->setMedia($media);
-			$item->setMetadata($metadata);
-
-			return $item;	
-		}
-		else{
-			return false;
-		}
-	}
 }
