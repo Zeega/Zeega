2bcbadfef6f4625536262f76f168373e1df8b0d0
James Burns <james@metalab.harvard.edu>
2012-02-06 17:40:41 +0000

    clean up routing in dev and prod environments

--- web/app.php
+++ web/app.php
@@ -6,7 +6,6 @@ require_once __DIR__.'/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel('prod', true);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
 $kernel->handle(Request::createFromGlobals())->send();
