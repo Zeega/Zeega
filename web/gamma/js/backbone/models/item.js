46ed066d7a07c0418360585c773bc7130b68e342
Luis Brandao <luisbrandao@gmail.com>
2012-02-08 18:58:27 -0500

    Fixed conflicts after merging 'master' into 'widget'

--- web/gamma/js/backbone/models/item.js
+++ web/gamma/js/backbone/models/item.js
@@ -8,7 +8,7 @@ var Item = Backbone.Model.extend({
 	
 	url: function(){
 		// http://dev.zeega.org/jda/web/api/items/703493
-		return sessionStorage.getItem('hostname') + sessionStorage.getItem('directory') + "api/items";
+		return sessionStorage.getItem('hostname') + sessionStorage.getItem('directory') + "api/items/" + this.id;
 	},
 	
 	initialize : function()
