bf92dc6792c6b0bb818417196263daa1887a7a7c
Luis Brandao <luisbrandao@gmail.com>
2012-02-13 18:03:48 -0500

    Finished Twig extension for the header data ( closes #375 ); refactored the EditorController

--- src/Zeega/CoreBundle/Twig/Extensions/HeaderTwigExtension.php
+++ src/Zeega/CoreBundle/Twig/Extensions/HeaderTwigExtension.php
@@ -9,15 +9,32 @@ class HeaderTwigExtension extends \Twig_Extension
 {
 	protected $doctrine;
 
-	public function __construct(Registry $doctrine)
+	public function __construct($doctrine, $securityContext)
 	{
         $this->doctrine = $doctrine;
+		$this->securityContext = $securityContext;
     }
 
     public function getGlobals()
     {
+		$user = $this->securityContext->getToken()->getUser();
+		
+		$sites = $this->doctrine->getRepository('ZeegaDataBundle:Site')->findSiteByUser($user->getId());
+		$site = $sites[0];
+		
+		$projects = $this->doctrine
+						 ->getRepository('ZeegaDataBundle:Project')
+						 ->findProjectsBySiteAndUser($site->getId(),$user->getId());
+        		
         return array(
-            'site' => new Site());
+            'site' => $site,
+			'title'=>$site->getTitle(),
+			'short'=>$site->getShort(),
+			'num_sites'=>count($sites),
+			'user_id' => $user->getId(),
+			'myprojects'=> $projects,
+			'displayname' => $user->getDisplayName(),
+			);
     }
 
 	public function getName()
