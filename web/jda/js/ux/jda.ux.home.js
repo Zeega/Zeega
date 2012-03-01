857de9a2aac9a421d6e9a4cba7943782541ca2fe
kanarinka <kanarinka@ikatun.org>
2012-03-01 02:00:36 +0000

    home page search working

--- web/jda/js/ux/jda.ux.home.js
+++ web/jda/js/ux/jda.ux.home.js
@@ -1,17 +1,17 @@
 
-
-
-
-
-
-
-
-
 $(document).ready(function() {
-	
 
-	$('#search-bar').find('input').focus(function(){$(this).attr('value','');});
-
-
+	$('#home-search-bar').find('input').focus(function(){$(this).attr('value','');});
+	$('#home-search-bar').find('input').bind('keypress', function(e) {
+        if(e.keyCode==13){
+            var query = $('#home-search-bar').find('input').val();
+			window.location = 'search#q=' + query;
+			return false;
+        }
+	});
+	$('#go-button').click(function(){
+		var query = $('#home-search-bar').find('input').val();
+		window.location = 'search#q=' + query;
+	});
 
 });
