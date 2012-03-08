0d91beae5ca1fedb13f57047991e57703d56bdd4
Luis Brandao <luisbrandao@gmail.com>
2012-03-08 11:50:32 -0500

    Removed ununsed symfony bundles

--- src/Zeega/ApiBundle/ZeegaApiBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Zeega\ApiBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ZeegaApiBundle extends Bundle
-{
-}
--- src/Zeega/IngestBundle/ZeegaIngestBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Zeega\IngestBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ZeegaIngestBundle extends Bundle
-{
-}
