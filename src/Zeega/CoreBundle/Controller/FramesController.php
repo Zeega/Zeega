7f5c396d6e886f8d1de1cffba61b5c67bae0c3ab
Joseph Bergen <jbergen@gmail.com>
2012-04-09 19:29:38 +0000

    created images/frames folder

--- src/Zeega/CoreBundle/Controller/FramesController.php
+++ src/Zeega/CoreBundle/Controller/FramesController.php
@@ -102,7 +102,7 @@ class FramesController extends Controller
     {
     	$em=$this->getDoctrine()->getEntityManager();
     	$frame=$em->getRepository('ZeegaDataBundle:Frame')->find($frame_id);
-		exec('/opt/webcapture/webpage_capture -t 50x50 -crop ' .$this->container->getParameter('hostname') .$this->container->getParameter('directory') .'frame/'.$frame_id.'/view '.$this->container->getParameter('path').'frames',$output);
+		exec('/opt/webcapture/webpage_capture -t 50x50 -crop ' .$this->container->getParameter('hostname') .$this->container->getParameter('directory') .'frame/'.$frame_id.'/view '.$this->container->getParameter('path').'images/frames',$output);
 		$url=explode(':/var/www/',$output[4]);
 		$frame->setThumbnailUrl($this->container->getParameter('hostname') . $url[1]);
 		$em->persist($frame);
