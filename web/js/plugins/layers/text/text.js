8084f3891a3ba3f6693509f6d5859c11d8d89851
Joseph Bergen <jbergen@gmail.com>
2012-04-17 16:12:12 +0000

    text layer opacity issues should be solved

--- web/js/plugins/layers/text/text.js
+++ web/js/plugins/layers/text/text.js
@@ -104,14 +104,16 @@
 		render : function()
 		{
 			var style = {
-				'color' : this.model.get('attr').color,
-				'backgroundColor' : 'rgba('+ this.model.get('attr').backgroundColor.toRGB() +','+ (this.attr.backgroundColorOpacity || 1) +')',
+				'color' : 'rgba('+ this.model.get('attr').color.toRGB() +','+ (this.model.get('attr').colorOpacity || 1) +')',
+				'backgroundColor' : 'rgba('+ this.model.get('attr').backgroundColor.toRGB() +','+ (this.model.get('attr').backgroundColorOpacity || 1) +')',
 				'opacity' : this.model.get('attr').opacity,
 				'fontSize' : this.model.get('attr').fontSize +'%',
 				'padding' : this.model.get('attr').padding +'%',
 				'whiteSpace' : 'nowrap'
 			}
+			console.log(this.model.get('attr'))
 			console.log('color: '+ style.backgroundColor)
+			console.log(this.model)
 
 			$(this.el).html( _.template( this.getTemplate(), this.model.get('attr') ) ).css( style );
 			
