e0b86e4a8c4baeadff3251d691a99dd916dec0a9
Joseph Bergen <jbergen@gmail.com>
2012-04-17 22:16:34 +0000

    rectangle layers now render in the frame player and show up in thumbnails. fixes #578

--- web/js/app/zeega.frame-player.js
+++ web/js/app/zeega.frame-player.js
@@ -16,13 +16,14 @@ $(document).ready(function(){
 	_.each( frameData.layers, function(layerID){
 		var layer = _.find( layersData, function(layer){ return layer.id == layerID });
 		
-		if( !_.isUndefined( layer.attr.thumbnail_url) && layer.attr.thumbnail_url != '' ) drawLayer( layer );
+		if( !_.isUndefined( layer.attr.thumbnail_url) && layer.attr.thumbnail_url != '' ) drawLayerThumbnail( layer );
+		else if( layer.type == 'Rectangle' ) drawRectangle( layer ); // this is a bandaid
 	})
 	
 	
 })
 
-function drawLayer( layer )
+function drawLayerThumbnail( layer )
 {
 	var visual = $('<img src="'+ layer.attr.thumbnail_url +'"/>').css({
 		'position':'absolute',
@@ -31,6 +32,21 @@ function drawLayer( layer )
 		'width':layer.attr.width+'%',
 		//'height':layer.attr.height+'%',
 		'opacity':layer.attr.opacity
+	});
+	
+	$('#zeega-player').append(visual);
+}
+
+function drawRectangle( layer )
+{
+	var visual = $('<div>').css({
+		'position':'absolute',
+		'left':layer.attr.left+'%',
+		'top':layer.attr.top+'%',
+		'width':layer.attr.width+'%',
+		'height':layer.attr.height+'%',
+		'opacity':layer.attr.opacity,
+		'background-color':layer.attr.backgroundColor
 	});
 	
 	$('#zeega-player').append(visual);
