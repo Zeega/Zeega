7e9c8464d0c181a314e54f9f33b3eaf8d3b74b53
Joseph Bergen <jbergen@gmail.com>
2012-04-18 03:09:02 +0000

    updated to hex helper

--- web/js/helpers/zeega.helpers.js
+++ web/js/helpers/zeega.helpers.js
@@ -102,4 +102,12 @@ String.prototype.toRGB = function()
 
 function rgbToHex(R,G,B) {return toHex(R)+toHex(G)+toHex(B)}
 
+function toHex(n) {
+ n = parseInt(n,10);
+ if (isNaN(n)) return "00";
+ n = Math.max(0,Math.min(n,255));
+ return "0123456789ABCDEF".charAt((n-n%16)/16)
+      + "0123456789ABCDEF".charAt(n%16);
+}
+
 
