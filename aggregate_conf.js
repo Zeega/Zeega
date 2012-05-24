65b093f75ade530c1f72bab5becae7592991713d
Luis Brandao <luisbrandao@gmail.com>
2012-05-24 20:45:23 +0000

    Fixed merge related bug on the minify configuration

--- aggregate_conf.js
+++ aggregate_conf.js
@@ -4,12 +4,12 @@ module.exports = {
 	},
 	convert:[
 		{
-		    input: 'web/js/loaders/editor.js',
+		    input: 'web/js/loaders/editor_min.js',
 		    output: 'web/js_min/editor.js',
 		    message:"Don't forget to check the JS paths in /src/Zeega/CoreBundle/Resources/views/Editor/editor.html.twig"
 		},
 		{
-		    input: 'web/js/loaders/browser.js',
+		    input: 'web/js/loaders/browser_min.js',
 		    output: 'web/js_min/browser.js',
 		    message:"Don't forget to check the JS paths in /src/Zeega/CoreBundle/Resources/views/Editor/browser.html.twig"
 		}
