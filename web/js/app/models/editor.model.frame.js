a2a9f04147d7b81fc8ce0ff0810e66b952d88d29
Joseph Bergen <jbergen@gmail.com>
2012-06-28 15:50:39 +0000

    error added to help identify and eliminate the destructo bug

--- web/js/app/models/editor.model.frame.js
+++ web/js/app/models/editor.model.frame.js
@@ -103,6 +103,17 @@
 			
 			}
 		},
+		
+		validate : function( attrs )
+		{
+			var flag = false;
+			_.each(attrs.layers, function(layerID){ if( !_.isNumber(layerID) ) flag = true });
+			if(flag)
+			{
+				alert('There was an error with your project :(/nplease email bugs@zeega.org and describe what you were doing that led to this error.\nPlease refresh your browser. Your last edit may not have saved. We apologize for the inconvenience.');
+				return 'layer array update error!';
+			}
+		}
 	
 
 	});
