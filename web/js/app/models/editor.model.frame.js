cbfb53ca7fabb94f7f15021691ec33d86d9d0c65
Joseph Bergen <jbergen@gmail.com>
2012-06-29 15:19:08 +0000

    validation updated to be more accurate

--- web/js/app/models/editor.model.frame.js
+++ web/js/app/models/editor.model.frame.js
@@ -107,15 +107,13 @@
 		validate : function( attrs )
 		{
 			console.log('VALIDATE LAYER ARRAY',attrs.layers+'');
-			var flag = false;
-			_.each(attrs.layers, function(layerID){
-				if( !_.isNumber(layerID) && layerID != false ) flag = true
-			});
-			if(flag)
+			
+			if( attrs.layers.length > 1 && _.include(attrs.layers,false))
 			{
 				alert('There was an error with your project :(/nplease email bugs@zeega.org and describe what you were doing that led to this error.\nPlease refresh your browser. Your last edit may not have saved. We apologize for the inconvenience.');
 				return 'layer array update error!';
 			}
+			
 		}
 	
 
