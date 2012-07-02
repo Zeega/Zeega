38beeb04ac651515ab82a6dd65eed185de95bacc
Joseph Bergen <jbergen@gmail.com>
2012-07-02 14:55:06 +0000

    fixed validation of empty frames

--- web/js/app/models/editor.model.frame.js
+++ web/js/app/models/editor.model.frame.js
@@ -108,7 +108,7 @@
 		{
 			console.log('VALIDATE LAYER ARRAY',attrs.layers+'');
 			
-			if( attrs.layers.length > 1 && _.include(attrs.layers,false))
+			if( !_.isNull(attrs.layers) && attrs.layers.length > 1 && _.include(attrs.layers,false))
 			{
 				alert('There was an error with your project :(/nplease email bugs@zeega.org and describe what you were doing that led to this error.\nPlease refresh your browser. Your last edit may not have saved. We apologize for the inconvenience.');
 				return 'layer array update error!';
