629c77a686ed9a48012aafb8041793cd7a3ecc8d
Luis Brandao <luisbrandao@gmail.com>
2012-07-11 15:34:45 -0400

    Delete unused views; moved library to the Discovery bundle

--- src/Zeega/DiscoveryBundle/Controller/DiscoveryController.php
+++ src/Zeega/DiscoveryBundle/Controller/DiscoveryController.php
@@ -1,5 +1,5 @@
 <?php
-namespace Zeega\CoreBundle\Controller;
+namespace Zeega\DiscoveryBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Zeega\DataBundle\Entity\Item;
@@ -17,114 +17,10 @@ use Doctrine\ORM\EntityRepository;
 
 use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 
-class EditorController extends Controller
+class DiscoveryController extends Controller
 {
-    public function homeAction()
-    {
-        $session = $this->getRequest()->getSession();
-        $site = $session->get('site');
-        
-        if(isset($site))
-        {
-            return $this->forward('ZeegaCoreBundle:Editor:site',array('short'=>$site->getShort()),array());
-        }
-		else
-		{
-		    $user = $this->get('security.context')->getToken()->getUser();
-    		$sites = $user->getSites();
-    		
-    		if(isset($sites) && count($sites) > 0)
-    		{
-    		    return $this->forward('ZeegaCoreBundle:Editor:site',array('short'=>$sites[0]->getShort()),array());
-    		}
-		}
-		
-		// by default go home - this should never happen
-		return $this->forward('ZeegaCoreBundle:Editor:site',array('short'=>'home'),array());
-    }
-
-	public function siteAction($short)
+	public function libraryAction($short)
 	{
-		$user = $this->get('security.context')->getToken()->getUser();
-		$site = $this->getDoctrine()->getRepository('ZeegaDataBundle:Site')->findOneByShort($short);
-
-        if(!isset($site))
-        {
-            $site = $this->getDoctrine()->getRepository('ZeegaDataBundle:Site')->findOneByShort('home');
-        }
-        
-        $projects = $this->getDoctrine()->getRepository('ZeegaDataBundle:Project')->findProjectsBySite($site->getId());
-
-		$session = $this->getRequest()->getSession();
-
-        // store an attribute for reuse during a later user request
-        $session->set('site', $site);
-
-		return $this->render('ZeegaCoreBundle:Editor:site.html.twig', array('allprojects' => $projects, 'page'=>'site',));
+		return $this->render('ZeegaDiscoveryBundle:Discovery:library.html.twig', array('page'=>'editor'));
 	}
-
-	public function browserAction($short)
-	{
-		return $this->render('ZeegaCoreBundle:Editor:browser.html.twig', array('page'=>'editor'));
-	} 
-	
-	public function editorAction($short,$id)
-	{	
-		$user = $this->get('security.context')->getToken()->getUser();
-		
-		$site = $this->getDoctrine()->getRepository('ZeegaDataBundle:Site')->findOneByShort($short);
-		$sequences = $this->getDoctrine()->getRepository('ZeegaDataBundle:Sequence')->findBy(array("project_id" => $id));
-					
-		$project = $this->getDoctrine()->getRepository('ZeegaDataBundle:Project')->findOneById($id);
-
-		$projectLayers =  $this->getDoctrine()->getRepository('ZeegaDataBundle:Layer')->findBy(array("project_id" => $id));
-
-		$sequence = $sequences[0];
-		
-		// create project from collections browser
-		$params = array('site'=>$site->getId());
-		$session = $this->getRequest()->getSession();
-		$collection_id = $session->get("collection_id");
-		
-		if(isset($collection_id))
-		{
-			$params['collection'] = $collection_id;
-			
-			$session->remove("collection_id"); // reads and deletes from session
-		}
-		else
-		{
-			$collection_id = -1;
-		}
-		
-		$params["exclude_content"] = "Collection";
-		$params["user"] = -1;
-		$params["site"] = $site->getId();
-		
-		$items = $this->forward('ZeegaApiBundle:Items:getItemsFilter', $params)->getContent();
-		$projectData = $this->forward('ZeegaApiBundle:Projects:getProject', array("id" => $id))->getContent();
-		
-		$userCollections = $this->getDoctrine()->getRepository('ZeegaDataBundle:Item')->findUserCollections($user->getId(), $site->getId());
-		
-		return $this->render('ZeegaCoreBundle:Editor:editor.html.twig', array(
-				'projecttitle'   => $project->getTitle(),
-				'projectid'   =>$project->getId(),
-				'project'   =>$project,
-				'sequence'=>$sequence,
-				'sequences'=>$sequences,
-				'projectLayers' => $projectLayers,
-           		'page'=>'editor',
-				'results' => $items,
-				'collection_id' => $collection_id,
-				'project_data' => $projectData,
-				'user_collections' => $userCollections,
-			));
-	} 
-	
-	public function faqAction()
-	{
-	    $user = $this->get('security.context')->getToken()->getUser();
-
-		return $this->render('ZeegaCoreBundle:Editor:faq.html.twig', array('page'=>'faq'));
-    } 
 }
