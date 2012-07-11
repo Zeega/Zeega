b2e9ee82665dfa9a8d100953055a709004579583
Joseph Bergen <jbergen@gmail.com>
2012-07-11 15:24:28 +0000

    rendering issue when going to play is hidden

--- web/js/app/zeega.js
+++ web/js/app/zeega.js
@@ -675,13 +675,15 @@ this.zeega = {
 		this.exportProject();
 		this.unrenderFrame( this.currentFrame );
 		this.player = new Player2($('body'));
-		this.player.loadProject(this.exportProject(), {sequenceID: parseInt(this.currentSequence.id), frameID : parseInt(this.currentFrame.id) } )
-		
+		this.player.loadProject(this.exportProject(), {sequenceID: parseInt(this.currentSequence.id), frameID : parseInt(this.currentFrame.id) } );
+		console.log('update background color')
+		$('body').css({'background':'#000'});
 	},
 	
 	restoreFromPreview : function()
 	{
 		this.previewMode = false;
+		$('body').css({'background':'#333'});
 		this.renderFrame( this.currentFrame );
 	},
 
