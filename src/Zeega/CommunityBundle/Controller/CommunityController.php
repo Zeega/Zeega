ddbbf264e4c0dbefaf19fa92b6f77351fc4c5633
Luis Brandao <luisbrandao@gmail.com>
2012-07-12 08:58:29 -0400

    Added community bundle views

---
+++ src/Zeega/CommunityBundle/Controller/CommunityController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Zeega\CommunityBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class CommunityController extends Controller
+{
+    public function aboutAction()
+    {
+        return $this->render('ZeegaCommunityBundle:About:about.html.twig');
+    }
+    
+    public function authorAction()
+    {
+        return $this->render('ZeegaCommunityBundle:Author:author.html.twig');
+    }
+    
+    public function homeAction()
+    {
+        return $this->render('ZeegaCommunityBundle:Home:home.html.twig');
+    }
+    
+    public function topicsAction()
+    {
+        return $this->render('ZeegaCommunityBundle:Topics:topics.html.twig');
+    }
+    
+}
