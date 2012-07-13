94b9b27377cd4629ad4c45ebda7fa7fdc93ff8ca
Luis Brandao <luisbrandao@gmail.com>
2012-07-13 12:23:11 -0400

    Moved the Sequences controller to the ApiBundle

--- web/js/app/models/editor.model.sequence.js
+++ web/js/app/models/editor.model.sequence.js
@@ -9,7 +9,7 @@
 		url : function()
 		{
 			if ( this.isNew() ) return zeega.app.url_prefix + 'api/projects/'+ zeega.app.project.id +'/sequences';
-			return zeega.app.url_prefix+'sequences/' + this.id;
+			return zeega.app.url_prefix+'api/sequences/' + this.id;
 		},
 		
 		defaultAttr : {
