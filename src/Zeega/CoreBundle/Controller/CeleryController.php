bd532d92a00f949778906bffd0b586d63959a059
Luis Brandao <luisbrandao@gmail.com>
2012-07-17 17:38:42 -0400

    Messaging

--- src/Zeega/CoreBundle/Controller/CeleryController.php
+++ src/Zeega/CoreBundle/Controller/CeleryController.php
@@ -7,11 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityRepository;
 
+
 class CeleryController extends Controller
 {
-	public function queueAction()
+    public function queueAction()
 	{
-	    $msg = array('user_id' => 1235, 'image_path' => '/path/to/new/pic.png');
+	    $id = uniqid();
+	    $msg = array("id" => $id,  "task" => "tasks.add", "args" => array(1,2));
         $this->get('old_sound_rabbit_mq.celery_task_producer')->publish(json_encode($msg), 'celery');
+        return $id;
+    }
+    
+    public function statusAction($id)
+	{
     } 
 }
