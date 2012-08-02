257a336aa6324cfab588e9d830b267670f55a925
Luis Brandao <luisbrandao@gmail.com>
2012-08-02 14:28:55 -0400

    Removed unused methods from the RegistrationController

--- src/Zeega/UserBundle/Controller/RegistrationController.php
+++ src/Zeega/UserBundle/Controller/RegistrationController.php
@@ -14,11 +14,12 @@ class RegistrationController extends BaseController
         $formHandler = $this->container->get('fos_user.registration.form.handler');
         $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
 
-        $process = $formHandler->process($confirmationEnabled);
+        $process = $formHandler->process($confirmationEnabled); // validate the form data
         if ($process) 
         {
             $user = $form->getData();
             
+            // send a confirmation email to the user
             if ($confirmationEnabled) 
             {
                 $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
@@ -28,45 +29,14 @@ class RegistrationController extends BaseController
             {
                 $sequence = 'fos_user_registration_confirmed';
             }
-            return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register_success.html.'.$this->getEngine(), array('user'=>$user));
+            
+            // user registration successful -> render the success page
+            return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register_success.html.'.$this->getEngine(), array());
         }
 		
+		// user registration in progress or there are errors on the form -> render the registration page
         return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
             'form' => $form->createView(),
             'theme' => $this->container->getParameter('fos_user.template.theme')));
-    }
-    
-    public function successAction()
-    {
-	    return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register_success.html.'.$this->getEngine(), array());
-    }
-
-    /**
-     * Tell the user to check his email provider
-     */
-    public function checkEmailAction()
-    {
-        $email = $this->container->get('session')->get('fos_user_send_confirmation_email/email');
-        $this->container->get('session')->remove('fos_user_send_confirmation_email/email');
-        $user = $this->container->get('fos_user.user_manager')->findUserByEmail($email);
-
-        if (null === $user) {
-            throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
-        }
-
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:checkEmail.html.'.$this->getEngine(), array(
-            'user' => $user,
-        ));
-    }
-    
-    /**
-     * Tell the user his account is now confirmed
-     */
-    public function confirmedAction()
-    {
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:confirmed.html.'.$this->getEngine(), array(
-            'user' => $user,
-        ));
     }
 }
