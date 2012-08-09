0ad85cada2d58931bd53f92985a23b2ffd56d82c
James Burns <james@metalab.harvard.edu>
2012-08-09 21:14:58 +0000

    update home to load cards after pageload

--- web/js/app/zeega.home.js
+++ web/js/app/zeega.home.js
@@ -1,3 +1,6 @@
+
+
+
 this.zeegaDashboard = {
 	// break up logical components of code into modules.
 	module: function()
@@ -21,7 +24,68 @@ this.zeegaDashboard = {
 
 	init : function()
 	{
-		var _this = this;
+	
+	
+	
+	
+		var HomeDeck = Backbone.View.extend({
+			tagName : 'div',
+
+			render : function()
+			{
+				var _this=this;
+				_.each(homeCards,function(card){
+					console.log(card);
+					console.log(_.template(_this.getTemplate(), card ));
+					_this.$el.append( _.template(_this.getTemplate(), card ) );
+				});
+				return this;
+			},
+			
+			getTemplate : function()
+			{
+				var html = 
+					'<div class="section-divider">'+
+						'<div class="shadow"></div>'+
+							'<div class="content">'+
+								'<div class="container">'+
+									'<div class="row">'+
+										'<div class="span8 offset3">'+
+											'<h2><%= seperator %></h2>'+
+										'</div>'+
+										'<div class="span3"></div>'+
+									'</div>'+
+								'</div>'+
+							'<div class="arrow-down"><div class="striped-arrow"></div></div>'+
+						'</div>'+
+					'</div>'+
+	
+						'<div class="special-project" style="background: url(<%= url %>) no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">'+
+							'<div class="special-gradient">'+
+								'<div class="container">'+
+									'<div class="row">'+
+										'<div class="span10">'+
+											'<div class="valign">'+
+												'<div class="valign-content">'+
+													'<h1><%= title %></h1>'+
+													'<h2><%= subtitle %></h2>'+
+												'</div>'+
+											'</div>'+
+										'</div>'+
+									'</div>'+
+								'</div>'+
+							'</div>'+
+						'</div>';
+	
+				return html;
+			}
+		});
+		
+		
+		this.homeDeck = new HomeDeck();
+		$('#home-cards').append(this.homeDeck.render().el);
+		
+		
 		
 		/*
 		var Dashboard = zeegaDashboard.module("dashboard");
