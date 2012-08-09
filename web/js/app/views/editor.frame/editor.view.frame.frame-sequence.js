2a4afad9589971297aaba0fc5dc8fe592834730a
Joseph Bergen <jbergen@gmail.com>
2012-08-09 00:00:01 +0000

    frame hover menu activated again! woooo

--- web/js/app/views/editor.frame/editor.view.frame.frame-sequence.js
+++ web/js/app/views/editor.frame/editor.view.frame.frame-sequence.js
@@ -185,7 +185,7 @@
 			
 				"<li id='frame-thumb-<%= frameID %>' class='frame-thumb' style='background-image:url(\"<%= thumbURL %>\")'>"+
 					"<div class='frame-update-overlay'></div>"+
-					"<div style='width:0px;'><a href='#' class='menu-toggle'><span class='zicon zicon-gear orange'></span></a></div>"+
+					"<div style='width:0px;'><a href='#' class='menu-toggle'><i class='icon-cog icon-white'></i></a></div>"+
 					"<div class='well menu'>"+
 						"<ul class='nav nav-list'>"+
 							"<li><a href='#' data-action='duplicate'>Duplicate Frame</a></li>"+
