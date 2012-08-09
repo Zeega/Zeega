aea0681806ce95d0efb37e7f9e7282aca76d835d
Joseph Bergen <jbergen@gmail.com>
2012-08-09 15:05:27 +0000

    loader removes when all layers are complete. fixes #1023

--- web/js/app/zeega.player.js
+++ web/js/app/zeega.player.js
@@ -744,10 +744,7 @@ var Player2 = Backbone.View.extend({
 					.animate({width : this.loadedCount/this.model.get('layers').length * 100 +'%' },2000)
 					.animate({width : this.loadedCount*1.5/this.model.get('layers').length * 100 +'%' },100000);
 				
-				if(this.model.get('layers').length == this.loadedCount)
-				{
-					if( _this.has_played ) this.fadeOut();
-				}
+				if(this.model.get('layers').length == this.loadedCount) this.fadeOut();
 			},
 			
 			fadeOut : function()
