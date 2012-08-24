78a8e7d56de759554321e3db759b8bc40655e7bc
Joseph Bergen <jbergen@gmail.com>
2012-08-24 00:55:47 +0000

    frame view cleaned out

--- web/js/app/views/editor.frame/editor.view.frame.frame-sequence.js
+++ web/js/app/views/editor.frame/editor.view.frame.frame-sequence.js
@@ -11,7 +11,6 @@
 			this.model.on( 'change:thumbnail_url', this.onNewThumb, this );
 			this.model.on('focus', this.focus, this );
 			this.model.on('blur', this.blur, this );
-			this.model.on('refresh_view', this.refreshView, this);
 
 			this.model.on( 'thumbUpdateFail', function(){
 				_this.$el.find('.frame-update-overlay').hide()
@@ -26,80 +25,19 @@
 		blur : function()
 		{
 			$(this.el).removeClass('active-frame');
-			this.model.inFocus = false
+			this.model.inFocus = false;
 		},
 	
 		render: function()
 		{
-			var _this = this;
-
 			this.$el.html( this.getTemplate() );
-
 			this.makeDroppable();
-
-			this.$el.hover(function(){
-				$(_this.el).find('.frame-menu').show();
-			},function(){
-				$(_this.el).find('.menu-items').removeClass('open');
-				$(_this.el).find('.frame-menu').hide();
-			})
-
-
-			this.$el.click(function(){
-				_this.goToFrame();
-				return false;
-			})
-		
-			this.$el.find('.menu-toggle').click(function(){
-				_this.openDropdown();
-				return false;
-			})
-		
-			this.$el.find('.nav-list a').click(function(){
-			
-				switch($(this).data('action'))
-				{
-					case 'delete':
-						if(confirm('Delete Frame?'))
-							zeega.app.project.sequences.get(_this.model.sequenceID).frames.remove( _this.model );
-						break;
-				
-					case 'duplicate':
-						zeega.app.duplicateFrame( _this.model );
-						break;
-					
-					default:
-						console.log('not recognized')
-				}
-				event.stopPropagation();
-				
-				return false;
-			})
-		
-			//enable the hover when dragging DB items	
-			this.$el.hover(
-				//mouseover
-				function()
-				{
-					//only highlight the frame if something is being dragged into it
-					if( zeega.app.draggedItem == null ) $(this).find('.menu-toggle').show();
-				},
-				//mouseout
-				function()
-				{
-					$(this).find('.menu-toggle').hide();
-					$(this).find('.menu').hide();
-				}
-			
-			);
-			
+			this.delegateEvents();
 			return this;
 		},
 
 		makeDroppable : function()
 		{
-			console.log('%%		make droppable')
-			//frame droppable stuff
 			var _this = this;
 			this.$el.droppable({
 				//accept : '.database-asset-list',
@@ -127,24 +65,27 @@
 		},
 	
 		events : {
-			'mouseover'		: 'showGear',
-			'click .menu-toggle' : 'openDropdown'
-		},
-		
-		showGear : function()
-		{
-
+			'click'			: 'goToFrame',
+			'click .frame-action-menu a' : 'frameAction'
 		},
 
-		refreshView : function()
+		frameAction : function(e)
 		{
-			$(this.el).attr('id', 'frame-thumb-'+this.model.id)
-		},
-	
-		openDropdown : function()
-		{
-			$(this.el).find('.menu').show();
-			event.stopPropagation();
+			switch($(e.target).data('action'))
+			{
+				case 'delete':
+					if(confirm('Delete Frame?'))
+						zeega.app.project.sequences.get(this.model.sequenceID).frames.remove( this.model );
+					break;
+				
+				case 'duplicate':
+					zeega.app.duplicateFrame( _this.model );
+					break;
+					
+				default:
+					console.log('not recognized')
+			}
+			return false;
 		},
 	
 		goToFrame : function()
@@ -157,9 +98,9 @@
 		{
 			var _this = this;
 			//Update thumbnail in sequence display
-			if( $(this.el).is(':visible '))
+			if( this.$el.is(':visible '))
 			{
-				$(this.el).fadeOut('fast',function(){
+				this.$el.fadeOut('fast',function(){
 					$(this)
 						.css('background-image','url("'+ _this.model.get('thumbnail_url') +'")')
 						.fadeIn('fast');
@@ -168,8 +109,8 @@
 			}
 			else
 			{
-				$(this.el).css('background-image','url("'+ this.model.get('thumbnail_url') +'")');
-				$(this.el).find('.frame-update-overlay').hide();
+				this.$el.css('background-image','url("'+ this.model.get('thumbnail_url') +'")');
+				this.$el.find('.frame-update-overlay').hide();
 			}
 		},
 	
@@ -177,14 +118,15 @@
 		{
 			var html = 
 			
-					"<div style='width:0px;'><a href='#' class='menu-toggle'><i class='icon-cog icon-white'></i></a></div>"+
-					"<div class='frame-update-overlay'></div>"+
-					"<div class='well menu'>"+
-						"<ul class='nav nav-list'>"+
+					"<ul class='menu'>"+
+						"<a href='#' class='menu-toggle'><i class='icon-cog icon-white'></i></a>"+
+						"<ul class='frame-action-menu'>"+
 							"<li><a href='#' data-action='duplicate'>Duplicate Frame</a></li>"+
 							"<li><a href='#' data-action='delete'>Delete Frame</a></li>"+
 						"</ul>"+
-					"</div>";
+					"</ul>"+
+					"<div class='frame-update-overlay'></div>";
+
 
 			return html;
 		}
