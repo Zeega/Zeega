1c5b95fa42f29dc750320314b2de5214aedd97dd
Luis Brandao <luisbrandao@gmail.com>
2012-09-10 11:58:53 -0400

    Added Flickr pagination support

--- src/Zeega/ExtensionsBundle/Parser/Flickr/ParserFlickrTag.php
+++ src/Zeega/ExtensionsBundle/Parser/Flickr/ParserFlickrTag.php
@@ -13,13 +13,6 @@ class ParserFlickrTag extends ParserAbstract
 			Commons','Attribution-NonCommercial-NoDerivs Creative Commons','Attribution Creative Commons',
 			'Attribution-ShareAlike Creative Commons','Attribution-NoDerivs Creative Commons','No known copyright restrictions');
 	
-	private $itemParser;
-	
-	function __construct() 
-	{
-		$this->itemParser = new ParserFlickrPhoto();
-	}
-	
 	public function load($url, $parameters = null)
     {
         $loadCollectionItems = $parameters["load_child_items"];
@@ -27,58 +20,84 @@ class ParserFlickrTag extends ParserAbstract
 	    $setId = $regexMatches[1]; // bam
 	    
 		$f = new \Phpflickr_Phpflickr('97ac5e379fbf4df38a357f9c0943e140');
-		$searchResults =  $f->photos_search(array("tags"=>"planettakeout", "per_page" => 500, "tag_mode"=>"any", "extras"=>"description, license, date_upload, date_taken, owner_name, geo, tags, url_l, url_o"));
-
-		$collection = new Item();
 		
-		$collection->setTitle("Planet Takeout photos");
-		$collection->setDescription("Planet Takeout description");
-		$collection->setMediaType('Collection');
-	    $collection->setLayerType('Flickr');
-		$collection->setAttributionUri("http://www.flickr.com/photos/tags/planettakeout/");
+        $searchParameters = array(
+            "tags"=>"planettakeout", 
+            "per_page" => 500, 
+            "tag_mode"=>"any", 
+            "extras"=>"description, license, date_upload, date_taken, owner_name, geo, tags, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o", 
+            "page"=>1
+            );
 
-        $collection->setChildItemsCount($setInfo["count_photos"]);
+        $currentPage = 1;
 
-		$collection->setMediaCreatorUsername("Planet Takeout");
-        $collection->setMediaCreatorRealname("Planet Takeout");
-		$collection->setMediaDateCreated(new \DateTime());
-		
-		if($loadCollectionItems == true)
-		{
+        $items = array();
+
+        while(1) {
+            $searchParameters["page"] = $currentPage;
+            $searchResults =  $f->photos_search($searchParameters);
             $photos = $searchResults['photo'];
+            $pages = $searchResults['pages'];
 
-    		if($photos)
-    		{
-    			foreach($photos as $photo)
-    			{
-                    $item = new Item();
-                    $tags = array();
+            foreach($photos as $photo) {
+                $item = new Item();
+                $tags = array();
 
-                    if(isset($photo['tags'])) $item->setTags(explode(" ", $photo['tags']));
-                    if(isset($photo['datetaken'])) $item->setMediaDateCreated(new DateTime($photo['datetaken']));
-                    if(isset($photo['ownername'])) $item->setMediaCreatorUsername($photo['ownername']);
-                    if(isset($photo['latitude'])) $item->setMediaGeoLatitude($photo['latitude']);
-                    if(isset($photo['longitude'])) $item->setMediaGeoLatitude($photo['longitude']);
+                if(isset($photo['tags'])) {
+                    $item->setTags(explode(" ", $photo['tags']));
+                }         
+                
+                if(isset($photo['datetaken'])) {
+                    $item->setMediaDateCreated(new DateTime($photo['datetaken']));
+                }    
+                
+                if(isset($photo['ownername'])) {
+                    $item->setMediaCreatorUsername($photo['ownername']);
+                }      
 
-                    if(isset($photo['license']))  $item->setLicense(self::$license[$photo['license']]);
-                    else $item->setLicense('All Rights Reserved');
+                if(isset($photo['latitude'])) {
+                    $item->setMediaGeoLatitude($photo['latitude']);
+                }   
+                
+                if(isset($photo['longitude'])) {
+                    $item->setMediaGeoLatitude($photo['longitude']); 
+                }  
+                
+                if(isset($photo['license'])) {
+                    $item->setLicense(self::$license[$photo['license']]);    
+                } else {
+                    $item->setLicense('All Rights Reserved');
+                }
 
-                    // $item->setThumbnailUrl($sizes['Square']['source']);
+                if(isset($photo["url_l"])) {
+                    $item->setUri($photo['url_l']);  
+                } else if (isset($photo["url_o"])) {
+                    $item->setUri($photo['url_o']);
+                } else if (isset($photo["url_m"])) {
+                    $item->setUri($photo['url_m']);
+                } else {
+                    $item->setUri($photo['url_s']);
+                }    
 
-                    $item->setTitle($photo['title']);
-                    $item->setAttributionUri("http://www.flickr.com/photos/".$photo["ownername"]."/".$photo["7939455914"]);
-                    $item->setArchive('Flickr'); 
-                    $item->setMediaType('Image');
-                    $item->setLayerType('Image');
-                    $item->setUri($photo['url_l']);
-                    $item->setChildItemsCount(0);
+                if(isset($photo["description"])) {
                     $item->setDescription($photo['description']);
-	    			
-                    $collection->addItem($item);
-    			}
-    		}
-		}
+                } 
+                
+                $item->setTitle($photo['title']);
+                $item->setAttributionUri("http://www.flickr.com/photos/".$photo["ownername"]."/".$photo["id"]);
+                $item->setArchive('Flickr'); 
+                $item->setMediaType('Image');
+                $item->setLayerType('Image');
+                $item->setChildItemsCount(0);
+                
+                array_push($items,$item);
+            }
+
+            if(($currentPage++ > $pages) || $currentPage > 10) {
+                break;
+            }
+        }		
 		
-		return parent::returnResponse($collection, true, true);
+		return parent::returnResponse($items, true, true);
 	}
 }
