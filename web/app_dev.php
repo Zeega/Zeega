4dad496f8251216906cde5115fabaaa6f5103ee9
Staging <luisbrandao@gmail.com>
2012-09-13 14:41:22 +0000

    Whitelist change

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,7 +13,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
         '::1',
-        '128.103.186.44'
+        '67.186.132.2'
     ))
 ) {
     header('HTTP/1.0 403 Forbidden');
