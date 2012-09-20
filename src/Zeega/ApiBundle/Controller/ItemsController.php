ca226745ff9a6b98647564130c179e7353dbd66e
Luis Brandao <luisbrandao@gmail.com>
2012-09-20 19:21:33 +0000

    Changed the 'collection to project' API response; changed 'solr search' response

--- src/Zeega/ApiBundle/Controller/ItemsController.php
+++ src/Zeega/ApiBundle/Controller/ItemsController.php
@@ -420,7 +420,26 @@ class ItemsController extends Controller
 				$frameId = (int)$item['id'];
 				$frameOrder[]=$frameId;
 				$frames[]=array("id"=>$frameId,"sequence_index"=>0,"layers"=>array($i),"attr"=>array("advance"=>0));
-				$layers[]=array("id"=>$i,"type"=>$item['layer_type'],"text"=>$item['text'],"attr"=>array("description"=>$item['description'],"title"=>$item['title'],"url"=>$item['uri'],"uri"=>$item['uri'],"thumbnail_url"=>$item['thumbnail_url'],"attribution_uri"=>$item['attribution_uri']));
+				$layers[]=array("id"=>$i,"type"=>$item['layer_type'],"text"=>$item['text'],
+                    "attr"=>array(
+                        "user_id"=>$item['user_id'],
+                        "description"=>$item['description'],
+                        "title"=>$item['title'],
+                        "uri"=>$item['uri'],
+                        "thumbnail_url"=>$item['thumbnail_url'],
+                        "attribution_uri"=>$item['attribution_uri'],
+                        "media_creator_username"=>$item['media_creator_username'],
+                        "media_creator_realname"=>$item['media_creator_realname'],
+                        "location"=>$item['location'],
+                        "media_date_created"=>$item['media_date_created'],
+                        "date_created"=>$item['date_created'],
+                        "tags"=>$item['tags'],
+                        "media_geo_latitude"=>$item['media_geo_latitude'],
+                        "media_geo_longitude"=>$item['media_geo_longitude'],
+                        "archive"=>$item['archive'],
+                        "media_type"=>$item['media_type'],
+                        "layer_type"=>$item['layer_type'] 
+                    ));
          	}
          }
          
@@ -429,7 +448,7 @@ class ItemsController extends Controller
                           "estimated_time"=>"Some time", 
                           "sequences"=>array(array('id'=>1,'frames'=>$frameOrder,"title"=>'none', 'attr'=>array("persistLayers"=>array()))),
                           'frames'=>$frames,
-                          'layers'=>$layers
+                          'layers'=>$layers,
                           );
          return new Response(json_encode($project));
     }
