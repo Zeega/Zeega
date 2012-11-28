dfc3c7cf9306205899a6c0eb98d6d9748555bb4f
Luis Brandao <luisbrandao@gmail.com>
2012-11-28 11:08:21 -0500

    Added check for status code

--- src/Zeega/ApiBundle/Listener/ApiResponseListener.php
+++ src/Zeega/ApiBundle/Listener/ApiResponseListener.php
@@ -1,19 +1,27 @@
 <?php
 
+/*
+* This file is part of Zeega.
+*
+* (c) Zeega <info@zeega.org>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace Zeega\ApiBundle\Listener;
 
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
-use Symfony\Component\Security\Core\SecurityContext;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Cookie;
 
 class ApiResponseListener
 {
     public function onKernelResponse(FilterResponseEvent $event)
     {
-        if (preg_match("/\/api\//",$event->getRequest()->getUri())) {
-            $content = $event->getResponse()->getContent();
-            $event->getResponse()->setContent(preg_replace("/(\n|\r|\s)*/","",$event->getResponse()->getContent()));
+        if($event->getResponse()->getStatusCode() == 200) {
+            if (preg_match("/\/api\//",$event->getRequest()->getUri())) {
+                $content = $event->getResponse()->getContent();
+                $event->getResponse()->setContent(preg_replace("/(\n|\r|\s)*/","",$event->getResponse()->getContent()));
+            }
         }
     }
 }
