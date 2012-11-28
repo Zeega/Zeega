5e35eb92fd8835b8eac9c62e5b9d4cb16445a06c
Luis Brandao <luisbrandao@gmail.com>
2012-11-28 11:07:06 -0500

    The base controller now extends the Symfony base controller

--- src/Zeega/CoreBundle/Controller/BaseController.php
+++ src/Zeega/CoreBundle/Controller/BaseController.php
@@ -11,21 +11,13 @@
 
 namespace Zeega\CoreBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\DependencyInjection\ContainerAware;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Bundle\DoctrineBundle\Registry;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller as SymfonyBaseController;
 
 /**
- * Controller is a simple implementation of a Controller based on the Symfony base controller.
- *
- * It provides methods to common features needed in Zeega controllers.
+ * Controller is a simple implementation of a Controller that extends the Symfony base controller.
  *
  */
-class BaseController extends ContainerAware
+class BaseController extends SymfonyBaseController
 {
     /**
      * Checks the logged user id matches the userId parameters and throws a not authorized
@@ -72,111 +64,5 @@ class BaseController extends ContainerAware
         }
 
         throw new AccessDeniedException();
-    }
-    
-    /**
-     * Generates a URL from the given parameters.
-     *
-     * @param string  $route      The name of the route
-     * @param mixed   $parameters An array of parameters
-     * @param Boolean $absolute   Whether to generate an absolute URL
-     *
-     * @return string The generated URL
-     */
-    protected function generateUrl($route, $parameters = array(), $absolute = false)
-    {
-        return $this->container->get('router')->generate($route, $parameters, $absolute);
-    }
-
-    /**
-     * Forwards the request to another controller.
-     *
-     * @param string $controller The controller name (a string like BlogBundle:Post:index)
-     * @param array  $path       An array of path parameters
-     * @param array  $query      An array of query parameters
-     *
-     * @return Response A Response instance
-     */
-    protected function forward($controller, array $path = array(), array $query = array())
-    {
-        return $this->container->get('http_kernel')->forward($controller, $path, $query);
-    }
-
-    /**
-     * Returns a RedirectResponse to the given URL.
-     *
-     * @param string  $url    The URL to redirect to
-     * @param integer $status The status code to use for the Response
-     *
-     * @return RedirectResponse
-     */
-    protected function redirect($url, $status = 302)
-    {
-        return new RedirectResponse($url, $status);
-    }
-
-    /**
-     * Returns a rendered view.
-     *
-     * @param string $view       The view name
-     * @param array  $parameters An array of parameters to pass to the view
-     *
-     * @return string The renderer view
-     */
-    protected function renderView($view, array $parameters = array())
-    {
-        return $this->container->get('templating')->render($view, $parameters);
-    }
-
-    /**
-     * Renders a view.
-     *
-     * @param string   $view       The view name
-     * @param array    $parameters An array of parameters to pass to the view
-     * @param Response $response   A response instance
-     *
-     * @return Response A Response instance
-     */
-    protected function render($view, array $parameters = array(), Response $response = null)
-    {
-        return $this->container->get('templating')->renderResponse($view, $parameters, $response);
-    }
-
-    /**
-     * Shortcut to return the request service.
-     *
-     * @return Request
-     */
-    protected function getRequest()
-    {
-        return $this->container->get('request');
-    }
-
-    /**
-     * Shortcut to return the Doctrine Registry service.
-     *
-     * @return Registry
-     *
-     * @throws \LogicException If DoctrineBundle is not available
-     */
-    protected function getDoctrine()
-    {
-        if (!$this->container->has('doctrine')) {
-            throw new \LogicException('The DoctrineBundle is not installed in your application.');
-        }
-
-        return $this->container->get('doctrine');
-    }
-
-    /**
-     * Gets a service by id.
-     *
-     * @param string $id The service id
-     *
-     * @return object The service
-     */
-    protected function get($id)
-    {
-        return $this->container->get($id);
     }
 }
