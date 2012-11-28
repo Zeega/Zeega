a74c80da460231bb29783b3a75c173b23b52748c
Luis Brandao <luisbrandao@gmail.com>
2012-11-28 11:12:10 -0500

    Style fixes

--- src/Zeega/CoreBundle/Controller/CoreController.php
+++ src/Zeega/CoreBundle/Controller/CoreController.php
@@ -18,16 +18,13 @@ use Doctrine\ORM\EntityRepository;
 
 class CoreController extends BaseController
 {
-    
-	public function faqAction()
-	{
-	    $user = $this->get('security.context')->getToken()->getUser();
-
-		return $this->render('ZeegaCoreBundle:Core:faq.html.twig', array('page'=>'faq'));
+    public function faqAction()
+    {
+        return $this->render('ZeegaCoreBundle:Core:faq.html.twig', array('page'=>'faq'));
     } 
-	
- 	public function unsupportedBrowserAction ()
- 	{
- 		return $this->render('ZeegaCoreBundle:Core:unsupportedbrowser.html.twig');
- 	}
+    
+    public function unsupportedBrowserAction ()
+    {
+        return $this->render('ZeegaCoreBundle:Core:unsupportedbrowser.html.twig');
+    }
 }
