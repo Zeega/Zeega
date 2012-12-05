4c9b1ddee2dcbefc3b850dd1908a3896f58c0bf5
Joseph Bergen <jbergen@gmail.com>
2012-12-05 23:37:44 +0000

    correctly determine if a layer is not in a frame when sorting

--- web/js/app/models/editor.model.frame.js
+++ web/js/app/models/editor.model.frame.js
@@ -109,17 +109,19 @@
 		{
 			var _this = this;
 			//remove layers not referenced from the collection
-			var brokenLayers = _.difference( _.map(this.layers.pluck('id'),function(id){ parseInt(id,10);}),layerIDArray );
-			var brokenLayerModelArray = _.map(brokenLayers, function(layerID){
-				return _this.layers.get(layerID);
+			var brokenLayerModelArray = this.layers.map(function(layer){
+				if( !_.contains(layerIDArray,layer.id) ) return layer;
+				return false;
 			});
-			this.layers.remove(brokenLayerModelArray,{silent:true});
+
+			this.layers.remove( _.compact(brokenLayerModelArray),{silent:true});
 
 			_.each(layerIDArray, function(layerID, i){
 				var layer = _this.layers.get(layerID);
 				$('#layer-visual-'+ layer.id).css('z-index', i);
 				layer.layerIndex = i;
 			});
+
 			this.layers.sort();
 			this.updateLayerOrder();
 		},
