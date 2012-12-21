1af2a1e1dd7da194d93f4cdec7f3d4136baa6298
Luis Brandao <luisbrandao@gmail.com>
2012-12-21 13:46:42 +0000

    Reverted config changes; fixed title for featured projects

--- src/Zeega/CommunityBundle/Controller/CommunityController.php
+++ src/Zeega/CommunityBundle/Controller/CommunityController.php
@@ -23,7 +23,7 @@ class CommunityController extends BaseController
     
     public function homeAction()
     {
-        $queryFields = array("id", "title_i", "thumbnail_url", "attribution_uri");
+        $queryFields = array("id", "title", "thumbnail_url", "attribution_uri");
         $projects = $this->forward('ZeegaApiBundle:Items:getItemsSearch', array(), array("collection" => 55121, "fields" => $queryFields))->getContent();
         return $this->render('ZeegaCommunityBundle:Home:home.html.twig',array('projects' => $projects));
     }
