c5c58c870a097bfee09ff11461520f21b72ff19e
Luis Brandao <luisbrandao@gmail.com>
2013-01-25 16:59:25 -0500

    Removed add new user button

--- src/Zeega/AdminBundle/AdminEntity/UserAdmin.php
+++ src/Zeega/AdminBundle/AdminEntity/UserAdmin.php
@@ -7,9 +7,15 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
 
 class UserAdmin extends Admin
 {
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->remove('create');
+    }
+
     protected function configureFormFields(FormMapper $formMapper)
     {
 		$roles = array('ROLE_USER' => 'User','ROLE_ADMIN'=>'Admin');
