46c876be995b33381db6324c874f00c83aeb7ca5
Luis Brandao <luisbrandao@gmail.com>
2013-01-29 18:12:26 -0500

    Getting profile pictures from twitter

--- src/Zeega/SocialBundle/Security/User/Provider/TwitterProvider.php
+++ src/Zeega/SocialBundle/Security/User/Provider/TwitterProvider.php
@@ -7,6 +7,8 @@ use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpKernel\Log\LoggerInterface;
+
 use \TwitterOAuth;
 use FOS\UserBundle\Doctrine\UserManager;
 use Symfony\Component\Validator\Validator;
@@ -21,12 +23,13 @@ class TwitterProvider implements UserProviderInterface
     protected $validator;
     protected $session;
 
-    public function __construct(TwitterOAuth $twitter_oauth, UserManager $userManager,Validator $validator, Session $session)
+    public function __construct(TwitterOAuth $twitter_oauth, UserManager $userManager,Validator $validator, Session $session, LoggerInterface $logger)
     {   
         $this->twitter_oauth = $twitter_oauth;
         $this->userManager = $userManager;
         $this->validator = $validator;
         $this->session = $session;
+        $this->logger = $logger;
     }   
 
     public function supportsClass($class)
@@ -68,6 +71,20 @@ class TwitterProvider implements UserProviderInterface
             $user->setEmail('');
             $user->setDisplayname($info->name);
 
+            try {
+                $this->twitter_oauth->decode_json = FALSE;
+                $twitterThumbnail = $this->twitter_oauth->get( "users/profile_image/luisfbrandao",array("size" => "original") );
+                $url = preg_match('/<a href="(.+)">/', $twitterThumbnail, $match);
+                $imageUrl = parse_url($match[1]);
+        
+                if( False !== $imageUrl ) {
+                     $user->setThumbUrl( $match[1] ); 
+                }
+                
+            } catch (Exception $e) {
+                $this->logger->err( 'Something went wrong getting the twitter profile image ' . $e->getMessage() );
+            }
+            
             $this->userManager->updateUser($user);
         }
 
