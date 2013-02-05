e3c3fb0a56462d4b8c5b667851790edaf7bae1e3
Luis Brandao <luisbrandao@gmail.com>
2013-02-05 14:34:18 -0500

    Removed idea field from registration

--- src/Zeega/UserBundle/Form/Type/RegistrationFormType.php
+++ src/Zeega/UserBundle/Form/Type/RegistrationFormType.php
@@ -12,16 +12,11 @@ class RegistrationFormType extends BaseType
         $builder
             ->add('display_name',null, array('required' => true))
             ->add('email', 'email')
-            ->add('idea')
             ->add('plainPassword', 'repeated', array(
                 'type' => 'password',
                 'options' => array('translation_domain' => 'FOSUserBundle'),
                 'invalid_message' => "The passwords don't match. Try again?",
                 'error_bubbling' => true
-            ))
-            ->add('locked', 'checkbox', array(
-                'label'     => "I've read and agree to Zeega's terms of use.",
-                'required'  => true,
             ));
     }
 
