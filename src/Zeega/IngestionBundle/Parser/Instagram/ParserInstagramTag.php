f2d050063f3aea35ef214a6db8ea1822cc37d832
Luis Brandao <luisbrandao@gmail.com>
2013-02-25 16:03:03 -0500

    Added result pagination

--- src/Zeega/IngestionBundle/Parser/Instagram/ParserInstagramTag.php
+++ src/Zeega/IngestionBundle/Parser/Instagram/ParserInstagramTag.php
@@ -20,70 +20,88 @@ class ParserInstagramTag extends ParserAbstract
 
         $items = array();
 
-        $apiUrl = "https://api.instagram.com/v1/tags/$tags/media/recent?access_token=1907240.f59def8.6a53e4264d87413a8e8cd431430b6e94";
+        $nextUrl = "https://api.instagram.com/v1/tags/$tags/media/recent?access_token=1907240.f59def8.6a53e4264d87413a8e8cd431430b6e94";
         
-        $itemsJson = file_get_contents($apiUrl,0,null,null);
+        while(;) {
+            $itemsJson = file_get_contents($nextUrl,0,null,null);
 
-        if(FALSE !== $checkForDuplicates) { // temp check for duplicates [with duplicated code]. transition to max_id / min_id later.
-            $em = $parameters["entityManager"];
-            $originalItems = $em->getRepository('ZeegaDataBundle:Item')->findUriByUserArchive($user->getId(), "Instagram");
-            
-            if(isset($originalItems)) {
-                $checkForDuplicates = TRUE;
-            } else {
-                $checkForDuplicates = FALSE;    
-            }
-        } else {
-            $checkForDuplicates = FALSE;
-        } 
-
-        if(null !== $itemsJson) {            
-            $apiItems = json_decode($itemsJson,true);
-
-            if(null !== $apiItems && is_array($apiItems) && array_key_exists("data", $apiItems)) {
-
-                foreach($apiItems["data"] as $apiItem) {
-                	if(TRUE === $checkForDuplicates) {
-                        if(TRUE === array_key_exists($apiItem['images']['standard_resolution']['url'], $originalItems)) {
-                            continue;
-                        }
-                    } 
-
-                    $item = new Item();
-                    $item->setTitle($apiItem["caption"]["text"]);
-                    $item->setMediaCreatorUsername($apiItem['user']['username']);
-                    $item->setMediaCreatorRealname($apiItem['user']['full_name']);
-                    $item->setMediaType('Image');
-                    $item->setLayerType('Image');
-                    $item->setArchive('Instagram');
-                    $item->setUri($apiItem['images']['standard_resolution']['url']);
-                    $item->setAttributionUri($apiItem['link']);
-                    $item->setMediaDateCreated(DateTime::createFromFormat('U', $apiItem['created_time']));                    
-                    $item->setThumbnailUrl($apiItem['images']['thumbnail']['url']);
-                    $item->setIdAtSource($apiItem['id']);
-                    
-                    $tags = $apiItem["tags"];                            
-                    if(isset($tags)) {
-                        $item->setTags($tags);
-                    }
-
-                    $location = $apiItem["location"];
-
-                    if(null !== $location && is_array($location)) {
-                    	if(array_key_exists("latitude", $location) && array_key_exists("longitude", $location)) {
-                    		$item->setMediaGeoLatitude($apiItem['location']['latitude']);
-                    		$item->setMediaGeoLongitude($apiItem['location']['longitude']);
-                    	}
-
-                    	if(array_key_exists("name", $location)) {
-                    		$item->setLocation($apiItem['location']['name']);	
-                    	}
-                    }
-
-                    array_push($items,$item);
+            if(FALSE !== $checkForDuplicates) { // temp check for duplicates [with duplicated code]. transition to max_id / min_id later.
+                $em = $parameters["entityManager"];
+                $originalItems = $em->getRepository('ZeegaDataBundle:Item')->findUriByUserArchive($user->getId(), "Instagram");
+                
+                if(isset($originalItems)) {
+                    $checkForDuplicates = TRUE;
+                } else {
+                    $checkForDuplicates = FALSE;    
                 }
+            } else {
+                $checkForDuplicates = FALSE;
+            } 
+
+            if(null !== $itemsJson) {            
+                $apiItems = json_decode($itemsJson,true);
+
+                if(null !== $apiItems && is_array($apiItems) && array_key_exists("data", $apiItems)) {
+
+                    foreach($apiItems["data"] as $apiItem) {
+                        if(TRUE === $checkForDuplicates) {
+                            if(TRUE === array_key_exists($apiItem['images']['standard_resolution']['url'], $originalItems)) {
+                                continue;
+                            }
+                        } 
+
+                        $item = new Item();
+                        $item->setTitle($apiItem["caption"]["text"]);
+                        $item->setMediaCreatorUsername($apiItem['user']['username']);
+                        $item->setMediaCreatorRealname($apiItem['user']['full_name']);
+                        $item->setMediaType('Image');
+                        $item->setLayerType('Image');
+                        $item->setArchive('Instagram');
+                        $item->setUri($apiItem['images']['standard_resolution']['url']);
+                        $item->setAttributionUri($apiItem['link']);
+                        $item->setMediaDateCreated(DateTime::createFromFormat('U', $apiItem['created_time']));                    
+                        $item->setThumbnailUrl($apiItem['images']['thumbnail']['url']);
+                        $item->setIdAtSource($apiItem['id']);
+                        
+                        $tags = $apiItem["tags"];                            
+                        if(isset($tags)) {
+                            $item->setTags($tags);
+                        }
+
+                        $location = $apiItem["location"];
+
+                        if(null !== $location && is_array($location)) {
+                            if(array_key_exists("latitude", $location) && array_key_exists("longitude", $location)) {
+                                $item->setMediaGeoLatitude($apiItem['location']['latitude']);
+                                $item->setMediaGeoLongitude($apiItem['location']['longitude']);
+                            }
+
+                            if(array_key_exists("name", $location)) {
+                                $item->setLocation($apiItem['location']['name']);   
+                            }
+                        }
+
+                        array_push($items,$item);
+                    }
+                }
+
+                if ( !array_key_exists("pagination", $apiItems) ) {
+                    if ( !array_key_exists("next_url", $apiItems) ) {
+                        break;
+                    }
+                    break;
+                }
+
+                $nextUrl = $apiItems["pagination"]["next_url"];
+
+                if( isset($nextUrl) ) {
+                    break;
+                }
+            } else {
+                break;
             }
         }
+ 
         return $this->returnResponse($items, true, true);
     }
 }
