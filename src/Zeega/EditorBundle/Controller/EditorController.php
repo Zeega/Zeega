7d116c428481a8fab9dbf26fdd95b52c109d53c5
Luis Brandao <luisbrandao@gmail.com>
2013-03-08 18:03:13 -0500

    Removed neweditor route

--- src/Zeega/EditorBundle/Controller/EditorController.php
+++ src/Zeega/EditorBundle/Controller/EditorController.php
@@ -79,25 +79,4 @@ class EditorController extends BaseController
 				));	
 		}
     }
-    
-    public function newEditorAction($id)
-    {   
-        $user = $this->get('security.context')->getToken()->getUser();
-
-        $project = $this->getDoctrine()->getRepository('ZeegaDataBundle:Project')->findOneById($id);
-        $userProjects = $this->getDoctrine()->getRepository('ZeegaDataBundle:Project')->findProjectsByUserSmall($user->getId());
-
-        $projectOwners = $project->getUsers();
-
-        $this->authorize($projectOwners[0]->getId());
-
-        $projectData = $this->forward('ZeegaApiBundle:Projects:getProject', array("id" => $id))->getContent();
-
-
-        return $this->render('ZeegaEditorBundle:Editor:neweditor.html.twig', array(
-            'project' =>$project,
-            'project_data' => $projectData,
-            'projects' => json_encode($userProjects)
-            ));
-    } 
 }
