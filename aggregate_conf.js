4b51b624bf981dbfaf002bbcd9f91514533e55c1
James Burns <james@metalab.harvard.edu>
2013-04-11 21:16:28 +0000

    clean up exception / page not found views for faster page load

--- aggregate_conf.js
+++ aggregate_conf.js
@@ -4,31 +4,36 @@ module.exports = {
 	},
 	convert:[
 		{
-		    input: 'web/js/loaders/home.js',
-		    output: 'web/js_min/home.js',
-		    message:"Don't forget to check the JS paths in /src/Zeega/CoreBundle/Resources/views/Community/home.html.twig"
+			input: 'web/js/loaders/home.js',
+			output: 'web/js_min/home.js',
+			message:"Don't forget to check the JS paths in /src/Zeega/CoreBundle/Resources/views/Community/home.html.twig"
 		},
 		{
-		    input: 'web/js/loaders/editor.js',
-		    output: 'web/js_min/editor.js',
-		    message:"Don't forget to check the JS paths in /src/Zeega/CoreBundle/Resources/views/Editor/editor.html.twig"
+			input: 'web/js/loaders/editor.js',
+			output: 'web/js_min/editor.js',
+			message:"Don't forget to check the JS paths in /src/Zeega/CoreBundle/Resources/views/Editor/editor.html.twig"
 		},
 		{
-		    input: 'web/js/loaders/discovery.js',
-		    output: 'web/js_min/discovery.js',
-		    message:"Don't forget to check the JS paths in /src/Zeega/DiscoveryBundle/Resources/views/Library/library.html.twig"
+			input: 'web/js/loaders/discovery.js',
+			output: 'web/js_min/discovery.js',
+			message:"Don't forget to check the JS paths in /src/Zeega/DiscoveryBundle/Resources/views/Library/library.html.twig"
 		},
 		{
-		    input: 'web/js/loaders/standalone-player.js',
-		    output: 'web/js_min/standalone-player.js',
-		    message:"Don't forget to check the JS paths in /src/Zeega/CoreBundle/Resources/views/Editor/player.html.twig"
+			input: 'web/js/loaders/standalone-player.js',
+			output: 'web/js_min/standalone-player.js',
+			message:"Don't forget to check the JS paths in /src/Zeega/CoreBundle/Resources/views/Editor/player.html.twig"
 		},
 		{
-		    input: 'web/js/loaders/widget.js',
-		    output: 'web/js_min/widget.js',
-		    message:"Don't forget to check the JS paths in /src/Zeega/CoreBundle/Resources/views/Editor/widget.html.twig"
+			input: 'web/js/loaders/widget.js',
+			output: 'web/js_min/widget.js',
+			message:"Don't forget to check the JS paths in /src/Zeega/CoreBundle/Resources/views/Editor/widget.html.twig"
 		},
+		{
+			input: 'web/js/loaders/header.js',
+			output: 'web/js_min/header.js',
+			message:""
+		}
 	
 
 	]
-}
+};
