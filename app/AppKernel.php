e5218ad9b46fca2e78f56c8b9b19f960a0e0ae97
Luis Brandao <luisbrandao@gmail.com>
2013-04-15 14:57:03 -0400

    Removed RabbitMq bundles and dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,7 +29,6 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle($this),
             new FOS\RestBundle\FOSRestBundle(),
             new Nelmio\SolariumBundle\NelmioSolariumBundle(),
-            new OldSound\RabbitMqBundle\OldSoundRabbitMqBundle(),
             new FOS\TwitterBundle\FOSTwitterBundle(),
             new FOS\FacebookBundle\FOSFacebookBundle(),
             new Snc\RedisBundle\SncRedisBundle(),
