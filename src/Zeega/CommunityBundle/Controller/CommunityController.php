c7da49a8e1f2f3bf2dbf1f4c087d69e1c1e33446
James Burns <james@zeega.com>
2013-06-07 05:05:51 +0000

    remove pages, update routing

--- src/Zeega/CommunityBundle/Controller/CommunityController.php
+++ src/Zeega/CommunityBundle/Controller/CommunityController.php
@@ -16,11 +16,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CommunityController extends BaseController
 {
-    public function authorAction()
-    {
-        return $this->render("ZeegaCommunityBundle:Author:author.html.twig");
-    }
-    
     public function homeAction()
     {
         $queryFields = array("description","tags","id", "title", "thumbnail_url", "attribution_uri", "views", "display_name","user_id", "user_thumbnail");
@@ -33,11 +28,6 @@ class CommunityController extends BaseController
         }
 
         return $this->render("ZeegaCommunityBundle:Home:home.html.twig",array("topZeega" => $topZeega));
-    }
-    
-    public function topicAction()
-    {
-        return $this->render("ZeegaCommunityBundle:Topic:topic.html.twig");
     }
     
     public function userAction($id)
@@ -61,65 +51,33 @@ class CommunityController extends BaseController
         }        
     }
     
-    public function missionAction()
-    {
-        return $this->render("ZeegaCommunityBundle:About:mission.html.twig");
-    }
-    
     public function privacyAction()
     {
         return $this->render("ZeegaCommunityBundle:About:privacy.html.twig");
     }
+
     public function termsAction()
     {
         return $this->render("ZeegaCommunityBundle:About:terms.html.twig");
     }
-    public function productionsAction()
-    {
-        return $this->render("ZeegaCommunityBundle:About:productions.html.twig");
-    }
+
     public function engineAction()
     {
         return $this->render("ZeegaCommunityBundle:About:engine.html.twig");
     }
+
     public function teamAction()
     {
         return $this->render("ZeegaCommunityBundle:About:team.html.twig");
     }
+
     public function contactAction()
     {
         return $this->render("ZeegaCommunityBundle:About:contact.html.twig");
-    }
-    public function newsAction()
-    {
-        return $this->render("ZeegaCommunityBundle:About:news.html.twig");
-    }
-    public function featuresAction()
-    {
-        return $this->render("ZeegaCommunityBundle:About:features.html.twig");
     }
 
     public function faqAction()
     {
         return $this->render("ZeegaCommunityBundle:About:faq.html.twig");
     } 
-    
-    
-    //LEGACY REROUTING –– TO BE MOVED
-    public function legacynewsAction()
-    {
-       return $this->redirect($this->generateUrl("ZeegaCommunityBundle_news"), 301);  
-    }
-    public function legacyhappeningsAction()
-    {
-         return $this->redirect($this->generateUrl("ZeegaCommunityBundle_about"), 301);  
-    }
-    public function legacyteamAction()
-    {
-        return $this->redirect($this->generateUrl("ZeegaCommunityBundle_team"), 301);  
-    }
-        public function legacyengineAction()
-    {
-       return $this->redirect($this->generateUrl("ZeegaCommunityBundle_engine"), 301);  
-    }
 }
