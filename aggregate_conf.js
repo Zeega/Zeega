e9f94454d2e5ff8faec33e7b2ae4b9810c0bddb6
James Burns <james@zeega.com>
2013-06-12 08:12:55 +0000

    Less to css

--- aggregate_conf.js
+++ aggregate_conf.js
@@ -8,11 +8,6 @@ module.exports = {
 			input: 'web/js/loaders/discovery.js',
 			output: 'web/js_min/discovery.js',
 			message:"Don't forget to check the JS paths in /src/Zeega/DiscoveryBundle/Resources/views/Library/library.html.twig"
-		},
-		{
-			input: 'web/js/loaders/header.js',
-			output: 'web/js_min/header.js',
-			message:""
 		}
 	
 
